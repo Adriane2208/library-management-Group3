d0e285600b48cbd5166b9c43cbc8345744bd6240
Adriane2208 <adrianengouabe@gmail.com>
2026-03-07 00:27:54 +0100

    Documentation de  API

--- src/main/java/com/library/library/controller/BookController.java
+++ src/main/java/com/library/library/controller/BookController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 import java.util.List;
 import java.util.Optional;
